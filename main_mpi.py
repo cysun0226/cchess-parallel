f66589842e123e2a62155f84311bfb8eee657727
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2020-01-01 03:21:14 +0800

    fix nod.v type bug

--- main_mpi.py
+++ main_mpi.py
@@ -1412,7 +1412,12 @@ class cchess_main(object):
         win_rate = 0
 
         if rank == 0:
-            actions_visits = [(act, nod.v[0]) for act, nod in self.mcts.root.child.items()]
+            actions_visits = []
+            for act, nod in self.mcts.root.child.items():
+                if type(nod.v) == int or type(nod.v) == float:
+                    actions_visits.append((act, nod.v))
+                else:
+                    actions_visits.append((act, nod.v[0]))
 
             sort_actions_visits = sorted(actions_visits, key=itemgetter(1), reverse=True)
 
