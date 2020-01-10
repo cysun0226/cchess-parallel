69d94dd9c002e822e9baec035a2bd6d20b3df7d0
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2020-01-10 13:18:19 +0800

    don't show tf warning

--- policy_value_network_gpus.py
+++ policy_value_network_gpus.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import numpy as np
 
 import os
+import logging
+logging.getLogger("tensorflow").setLevel(logging.WARNING)
 
 PS_OPS = ['Variable', 'VariableV2', 'AutoReloadVariable']
 
